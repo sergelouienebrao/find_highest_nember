7c45db334e1af757189c492c55e6023c7ea05d47
Serge <sergelouienebrao@gmail.com>
2024-10-16 00:12:07 +0800

    number inputs

--- find_highest_number.py
+++ find_highest_number.py
@@ -1,13 +1,19 @@
 # program to find the highest number
+def find_highest():
 
 # input five numbers from the user
+ num1 = int(input("Please input 1st number:"))
+ num2 = int(input("Please input 2nd number:"))
+ num3 = int(input("Please input 3rd number:"))         
+ num4 = int(input("Please input 4th number:"))
+ num5 = int(input("Please input 5th number:"))
 
 #rank each number to determine the highest 
 
 #print the highest number 
 
 #call the function to run the program
-
+find_highest()
 
 
 
