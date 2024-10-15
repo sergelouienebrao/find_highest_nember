d5bd9aa92659c699f92abf8dac2f8d3cfae3bf76
Serge <sergelouienebrao@gmail.com>
2024-10-16 00:00:40 +0800

    process

---
+++ find_highest_number.py
@@ -0,0 +1,14 @@
+# program to find the highest number
+
+# input five numbers from the user
+
+#rank each number to determine the highest 
+
+#print the highest number 
+
+#call the function to run the program
+
+
+
+
+
