cf8633a31291d8178c1d3fa42b1376dc43a02afe
Serge <sergelouienebrao@gmail.com>
2024-10-17 23:24:04 +0800

    if statements

--- find_highest_number.py
+++ find_highest_number.py
@@ -8,8 +8,21 @@ def find_highest():
  num4 = int(input("Please input 4th number:"))
  num5 = int(input("Please input 5th number:"))
 
-#rank each number to determine the highest 
+#rank each number to determine the highest
+ if num1 > num2:
+    highest = num1 
 
+ else:
+    highest = num2
+
+ if num3 > highest:
+    highest = num3 
+
+ if num4 > highest:
+    highest = num4
+
+ if num5 > highest:
+    highest = num5
 #print the highest number 
 
 #call the function to run the program
